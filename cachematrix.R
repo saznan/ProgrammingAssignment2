e6c4c034db4b1f924c3b44015151470f6bebceb6
saldesigns <raihan.salman@gmail.com>
2020-11-10 21:18:56 -0800

    My work on programming assignment 2: lexical scoping.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I took the makeVector function example provided and changed it
+## to caching a matrix's inverse. 
 
-## Write a short comment describing this function
+## This function caches the inverse of a matrix. I changed
+## the variable 'm' to 'invr' and changed all 'mean' values to 
+## 'inverse'.
 
 makeCacheMatrix <- function(x = matrix()) {
+    invr <- NULL
+    set <- function(y) {
+      x <<- y
+      invr <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) invr <<- inverse
+    getinverse <- function() invr
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+  }
 
-}
 
-
-## Write a short comment describing this function
+## This function, similar to the example provided, returns the
+## inverse of the special matrix. I changed the mean function
+## to solve() to get the inverse. And all 'mean' values I changed
+## to 'inverse'. 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+      invr <- x$getinverse()
+      if(!is.null(invr)) {
+      message("getting cached data")
+      return(invr)
+  }
+  data <- x$get()
+  invr <- solve(data, ...)
+  x$setinverse(invr)
+  invr
+  
 }
